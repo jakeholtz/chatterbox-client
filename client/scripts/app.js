3d523821f2d01898687d6c02d92b409f629f0292
Hack Reactor Student <student@hackreactor.com>
2017-04-08 16:30:08 -0700

    [Finished everything except befriendment]

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,10 +8,14 @@ var app = {};
 
 app.init = function(item){
   this.handleUsernameClick();
-  this.handleSubmit();
   this.roomName = null;
-  this.rooms = new Set();
-  setInterval(this.fetch(), 2000);
+  this.rooms = [];
+  app.fetch();
+  setInterval(app.fetch.bind(this), 2000);
+  $( "#submit_button" ).on('click', function(event) {
+      event.preventDefault()
+      app.handleSubmit($('#message').val());
+  });
 };
 
 
@@ -22,6 +26,8 @@ app.send = function(message){
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
+      // this.data.val('');
+      // this.fetch();
       console.log('chatterbox: Message sent');
     },
     error: function (data) {
@@ -30,12 +36,12 @@ app.send = function(message){
   });
 };
 
-
 app.fetch = function(){
   $.ajax({
     url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
     type: 'GET',
     contentType: 'application/json',
+    data: "order=-createdAt",
     success: function (data) {
       console.log('chatterbox: Message sent');
       app.showChats(data);
@@ -57,25 +63,38 @@ app.renderMessage = function(message){
   var $username = $(`<p class="username">${message.username}</p>`);
   var $text = $(`<span class="chatmessage">${message.text}</span>`)
   var $chatmessage = $('<div class="chat"></div>');
-  $('#chats').prepend($chatmessage);
+  $('#chats').append($chatmessage);
   $chatmessage.append($username);
   $chatmessage.append($text);
 };
 
 app.getRooms = function(data){
-  for (var obj in data.results) {
-    this.rooms.add(data.results[obj].roomname);
+  for (var prop in data.results) {
+    if (!_.contains(this.rooms, data.results[prop].roomname)) {
+      this.rooms.push(data.results[prop].roomname);
+    }
   }
   this.rooms = Array.from(this.rooms).sort();
   for (var room in this.rooms){
-    $('#roomselector').append(`<option>${this.rooms[room]}</option>`);
+    $('#roomselector').append(`<option value="${this.rooms[room]}">${this.rooms[room]}</option>`);
   }
-
-  console.log(this.rooms);
 }
 
 app.renderRoom = function(room){
   this.roomName = room;
+    $.ajax({
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+      app.showChats(data);
+      app.getRooms(data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
   // this.fetch();
   var divStart = '<div>';
   var divEnd = '</div>';
@@ -88,22 +107,27 @@ app.handleUsernameClick = function(friend){
 };
 
 
-app.handleSubmit = function(message){
-  return true;
-};
+app.handleSubmit = function(message) {
+    var message = {
+      username: window.location.search.substr(10),
+      text: message,
+      roomname: 'lobby'
+    };
 
+    app.send(message);
+
+    // Stop the form from submitting
+}
 
 app.showChats = function(data){
   if (this.roomName === null){
     for (var i = 0; i < data.results.length; i++){
       this.renderMessage(data.results[i]);
-      console.log(data.results[i]);
     }
   } else {
     for (var i = 0; i < data.results.length; i++){
       if (data.results[i].roomname == this.roomName){
         this.renderMessage(data.results[i]);
-        console.log(data.results[i]);
       }
     }
   }

fa65d7b1d0dd29e003ed7ec5c0d599d03e47cacf
Hack Reactor Student <student@hackreactor.com>
2017-04-07 17:03:55 -0700

    [Working changes - Passed 8 Tests]

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,28 @@
 // YOUR CODE HERE:
+document.write("Ashwin and Jake's Chatterbox");
+
+var app = {};
+  // url: 'http://parse.sfm6.hackreactor.com/2745f6eedad1770c6ebaf03f8a97cf0cc2f66706',
+  // dataType: 'JSON',
+  // headers: { "HeaderName": "4f44a6835e581124936858b658e8ea99e278d371" }
+
+app.init = function(item){ return true; };
+app.send = function(message){
+	this.type = 'POST';
+  this.data = JSON.stringify(message);
+  
+  $.ajax(this);
+};
+
+app.fetch = function(){
+  this.type = 'GET';
+  $.ajax(this);
+};
+
+app.clearMessages = function(){
+   $( "#chats" ).remove();
+};
+
+// app.renderMessage = function(message){
+//    $( "#chats" ).append('<div>'+message+'</div>');
+// };
